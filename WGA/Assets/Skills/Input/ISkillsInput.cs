90b4592b81afda9121bcfd0fe5b3620975abced0
NoopikxD <sashas_lob@mail.ru>
2018-02-21 21:59:08 +0300

    Directions +map

--- WGA/Assets/Skills/Input/ISkillsInput.cs
+++ WGA/Assets/Skills/Input/ISkillsInput.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-public enum Directions { Top, Bottom, Left, Right, Left_Top, Left_Bottom, Right_Top, Right_Bottom }
+public enum Directions { Top, Bottom, Left, Right, Left_Top, Left_Bottom, Right_Top, Right_Bottom, Map }
 
 interface ISkillsInput
 {
