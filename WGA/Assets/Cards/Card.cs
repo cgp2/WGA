5c3a5552f22e8ba6f2bebecc18e6190d6edb6283
NoopikxD <sashas_lob@mail.ru>
2018-02-25 19:12:01 +0300

    Тут мы сделали новый гит.игнор, перезалили весь проект, без меты и кэша и начали радоваться жизни и ускорили разработку в миллиард раз.

---
+++ WGA/Assets/Cards/Card.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public abstract class Card : MonoBehaviour
+{
+    public int Health;
+    public int Shield;
+    public int Attack;
+  
+    public Text Name;
+    public Text Description;
+
+    public string BattleCryName;
+    public string DeathRattleName;
+    public string AuraName;
+
+	// Use this for initialization
+	void Start ()
+    {
+		//well done
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
