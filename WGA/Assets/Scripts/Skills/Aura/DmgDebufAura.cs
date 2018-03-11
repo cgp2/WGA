a7f64cccc54d84cfa750ace761e9735906db4e2e
cgp2 <cgp2@yandex.ru>
2018-03-11 09:02:39 +0300

    strange hernia v2

---
+++ WGA/Assets/Scripts/Skills/Aura/DmgDebufAura.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Skills.Aura
+{
+    class DmgDebufAura : DmgBufAura
+    {
+        public DmgDebufAura() : base()
+        {
+            Name = "DMGDebufAura";
+            Description = "This aura reduce DMG to enemy cards";
+            Ally = false;
+
+            InputParametrs = new[] { "DMGBuf" };
+            InputValues = new[] { "-2" };
+            Dirs = new[] {Directions.Top};
+            Type = SkillType.Aura;
+
+            Input = new SkillsInput()
+            {
+                ParentFunctionName = Name,
+                InputParamsNames = InputParametrs,
+                InputParamsValues = InputValues,
+                Directions = Dirs
+            };
+        }
+
+
+    }
+}
---
+++ WGA/Assets/Scripts/Skills/DeathRattle/ShldDebufDR.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Assets.Scripts.Skills.DeathRattle
+{
+    class ShldDebufDR : ASkill
+    {
+        public ShldDebufDR()
+        {
+            Name = "ShldDebufDR";
+            Description = "This skill reduce SHLD to eneme cards on card death";
+            Ally = false;
+
+            InputParametrs = new[] { "ShldBuf" };
+            InputValues = new[] { "-5" };
+            Dirs = new[] { Directions.Map};
+            Type = SkillType.DeathRattle;
+
+            Input = new SkillsInput()
+            {
+                ParentFunctionName = Name,
+                InputParamsNames = InputParametrs,
+                InputParamsValues = InputValues,
+                Directions = Dirs
+            };
+        }
+
+        public override bool ExecuteSkill(SkillsInput input, int row, int col, int playerID, ref SlotBuff[,] bufMap)
+        {
+            var t = input;
+            var buffedSlots = GetCardSlotsInDirections(ref bufMap, input.Directions, playerID, row, col);
+
+            var n = Array.IndexOf(input.InputParamsNames, "ShldBuf");
+            var buf = input.InputParamsValues[n];
+            for (var i = 0; i < buffedSlots.Length; i++)
+            {
+                if (playerID == 0)
+                {
+                    if (Ally)
+                    {
+                        buffedSlots[i].StaticHPBufPlayer1 += int.Parse(buf);
+                    }
+                    else
+                    {
+                        buffedSlots[i].StaticShieldBufPlayer2 += int.Parse(buf);
+                    }
+                }
+                else
+                {
+                    if (Ally)
+                    {
+                        buffedSlots[i].StaticShieldBufPlayer2 += int.Parse(buf);
+                    }
+                    else
+                    {
+                        buffedSlots[i].StaticShieldBufPlayer1 += int.Parse(buf);
+                    }
+                }
+            }
+
+            ApplyBufToBufMap(buffedSlots, ref bufMap);
+
+            return true;
+        }
+
+        public override bool ReExecuteSkill(SkillsInput input, int row, int col, int playerID, ref SlotBuff[,] bufMap)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
