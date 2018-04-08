3b45e59718425acd14188e3dd15d39a503742b2d
cgp2 <cgp2@yandex.ru>
2018-04-08 22:03:40 +0300

    WTF IS HAPPENING

---
+++ WGA/Assets/Scripts/Controller.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Controller : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void MoveLeft()
+    {
+        Battle.Move(Directions.Left);
+    }
+
+    public void MoveRight()
+    {
+        Battle.Move(Directions.Right);
+    }
+
+    public void MoveTop()
+    {
+        Battle.Move(Directions.Top);
+    }
+
+    public void MoveBottom()
+    {
+        Battle.Move(Directions.Bottom);
+    }
+}
