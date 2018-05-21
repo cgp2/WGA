22c2de6e2171f7d3852cc3e56bfe314391759a8c
NoopikxD <sashas_lob@mail.ru>
2018-05-21 23:21:45 +0300

    Sometbhing

--- WGA/Assets/Scripts/Player/PlayerInfo.cs
+++ WGA/Assets/Scripts/Player/PlayerInfo.cs
@@ -8,19 +8,23 @@ using System.IO;
 [Serializable]
 public class PlayerInfo : MonoBehaviour {
     public string Name;
-    public Deck deck = new Deck();
-    public int level;
-    public int exp;
+    public Deck Deck = new Deck();
+    public int Level;
+    public int Exp;
+    public int ExpToNextLevel;
     //public Sprite avatar;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
         
 	}
 	// Update is called once per frame
 	void Update () {
 		
 	}
-    
+    public void SaveToFile()
+    {
+
+    }
 }
 
