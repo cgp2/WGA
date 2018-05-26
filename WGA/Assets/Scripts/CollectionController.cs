ae3d665cb42b327ac7e6e0fd7d375696319ef597
cgp2 <cgp2@yandex.ru>
2018-05-26 19:47:04 +0300

    collection fab

---
+++ WGA/Assets/Scripts/CollectionController.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Timeline;
+
+public class CollectionController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+
+	        //ItemGameObject is my prefab pointer that i previous made a public property  
+	        //and  assigned a prefab to it
+	       
+	    
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
