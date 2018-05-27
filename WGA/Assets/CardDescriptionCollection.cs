776e6b36a37e56c07e25c5fa59c06d9333bf2779
NoopikxD <sashas_lob@mail.ru>
2018-05-27 23:01:39 +0300

    Card description collection done

--- WGA/Assets/CardDescriptionCollection.cs
+++ WGA/Assets/CardDescriptionCollection.cs
@@ -5,16 +5,19 @@ using UnityEngine.UI;
 using UnityEngine.EventSystems;
 public class CardDescriptionCollection : MonoBehaviour, IPointerEnterHandler
 {
-    
-	// Use this for initialization
-	void Start () {
-    
-	}
+    GameObject CardDetail;
+    // Use this for initialization
+    void Start () {
+        CardDetail = GameObject.Find("CardDetail");
+    }
    public void OnPointerEnter(PointerEventData eventData)
     {
         var  CardDetail = GameObject.Find("CardDetail");
         var card = this.transform.GetComponent<Card>();
         CardDetail.transform.GetChild(0).GetComponent<Image>().sprite = card.Info.ShipSprite;
+        if(card.Info.ShipSprite==null)
+            CardDetail.transform.GetChild(0).GetComponent<Image>().color = new Color(255, 255, 255, 0);
+        else
         CardDetail.transform.GetChild(0).GetComponent<Image>().color = new Color(255, 255, 255, 255);
         CardDetail.transform.GetChild(1).GetComponent<Text>().text = card.Info.Name;
        
@@ -22,22 +25,23 @@ public class CardDescriptionCollection : MonoBehaviour, IPointerEnterHandler
         var obj = this.GetComponent<Card>();
         temp += obj.Info.Description;
         temp += "\n";
-        if(obj.Info.BattleCryNames!=null)
-        for (int i = 0; i < obj.Info.BattleCryNames.Length; i++)
+        if(obj.Data.BattleCryInputValue!=null)
+        for (int i = 0; i < obj.Data.BattleCryNames.Length; i++)
         {
-            temp += obj.Info.BattleCryNames[i] + ". Value: " + obj.Info.BattleCryValue[i] + "\n";
+            temp += obj.Info.BattleCryNames[i] + ". Value: " + obj.Data.BattleCryInputValue[i] + "\n";
         }
-        if(obj.Info.AuraNames!=null)
+        if(obj.Data.AurasNames!=null)
         for (int i = 0; i < obj.Info.AuraNames.Length; i++)
         {
-            temp += obj.Info.AuraNames[i] + ". Value: " + obj.Info.AuraInputValue[i] + "\n";
+            temp += obj.Info.AuraNames[i] + ". Value: " + obj.Data.AuraInputValue[i] + "\n";
         }
-        if (obj.Info.DeathRattleName != null)
+        if (obj.Data.DeathRattleNames != null)
         for (int i = 0; i < obj.Info.DeathRattleName.Length; i++)
         {
-            temp += obj.Info.DeathRattleName[i] + ". Value: " + obj.Info.DeathRattleInputValue[i] + "\n";
+            temp += obj.Info.DeathRattleName[i] + ". Value: " + obj.Data.DeathRattleInputValue[i] + "\n";
         }
-        temp += obj.Info.ActiveSkillName + ". Value: " + obj.Info.ActiveSkillInput.InputParamsValues[0] + "\n";
+        if(obj.Data.ActiveSkillName!=null)
+            temp += obj.Data.ActiveSkillName + ". Value: " + obj.Data.ActiveInputValue + "\n";
        
         CardDetail.transform.GetChild(2).GetComponent<Text>().text = temp;
     }
