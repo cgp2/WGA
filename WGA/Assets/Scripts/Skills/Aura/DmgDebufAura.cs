8a768afd1c147f2bcdca727f36a9eda3db3904cb
cgp2 <cgp2@yandex.ru>
2018-05-28 03:00:01 +0300

    collection

--- WGA/Assets/Scripts/Skills/Aura/DmgDebufAura.cs
+++ WGA/Assets/Scripts/Skills/Aura/DmgDebufAura.cs
@@ -27,6 +27,81 @@ namespace Assets.Scripts.Skills.Aura
             };
         }
 
+        public override bool ExecuteSkill(SkillsInput input, int row, int col, int playerID, ref SlotBuff[,] bufMap)
+        {
+            var t = input;
+            var buffedSlots = GetCardSlotsInDirections(ref bufMap, input.Directions, playerID, row, col);
+
+            var n = Array.IndexOf(input.InputParamsNames, "DMGBuf");
+            var buf = input.InputParamsValues[n];
+            for (var i = 0; i < buffedSlots.Length; i++)
+            {
+                if (playerID == 0)
+                {
+                    if (Ally == true)
+                    {
+                        buffedSlots[i].FloatingDMGBufPlayer1 -= int.Parse(buf);
+                    }
+                    else
+                    {
+                        buffedSlots[i].FloatingDMGBufPlayer2 -= int.Parse(buf);
+                    }
+                }
+                else
+                {
+                    if (Ally == true)
+                    {
+                        buffedSlots[i].FloatingDMGBufPlayer2 -= int.Parse(buf);
+                    }
+                    else
+                    {
+                        buffedSlots[i].FloatingDMGBufPlayer1 -= int.Parse(buf);
+                    }
+                }
+            }
+
+            ApplyBufToBufMap(buffedSlots, ref bufMap);
+
+            return true;
+        }
+
+        public override bool ReExecuteSkill(SkillsInput input, int row, int col, int playerID, ref SlotBuff[,] bufMap)
+        {
+            var t = input;
+            var buffedSlots = GetCardSlotsInDirections(ref bufMap, input.Directions, playerID, row, col);
+
+            var n = Array.IndexOf(input.InputParamsNames, "DMGBuf");
+            var buf = input.InputParamsValues[n];
+            for (var i = 0; i < buffedSlots.Length; i++)
+            {
+                if (playerID == 0)
+                {
+                    if (Ally == true)
+                    {
+                        buffedSlots[i].FloatingDMGBufPlayer1 += int.Parse(buf);
+                    }
+                    else
+                    {
+                        buffedSlots[i].FloatingDMGBufPlayer2 += int.Parse(buf);
+                    }
+                }
+                else
+                {
+                    if (Ally == true)
+                    {
+                        buffedSlots[i].FloatingDMGBufPlayer2 += int.Parse(buf);
+                    }
+                    else
+                    {
+                        buffedSlots[i].FloatingDMGBufPlayer1 += int.Parse(buf);
+                    }
+                }
+            }
+
+            ApplyBufToBufMap(buffedSlots, ref bufMap);
+
+            return true;
+        }
 
     }
 }
